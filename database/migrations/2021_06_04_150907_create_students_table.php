d3c56256a29244e51e254618f049bff532df34cf
Kevin Tulus <kevintulussilitonga@gmail.com>
2021-06-04 23:41:33 +0700

    Database

---
+++ database/migrations/2021_06_04_150907_create_students_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict');
+            $table->unsignedBigInteger('nis');
+            $table->integer('kelas');
+            $table->integer('b_inggris');
+            $table->integer('b_indonesia');
+            $table->integer('matematika');
+            $table->integer('fisika');
+            $table->integer('kimia');
+            $table->integer('biologi');
+            $table->integer('sejarah');
+            $table->integer('ekonomi');
+            $table->integer('sosiologi');
+            $table->integer('geografi');
+            $table->integer('pkn');
+            $table->integer('agama');
+            $table->integer('penjas');
+            $table->integer('komputer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
