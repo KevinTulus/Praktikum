e5e2e6c92171471bca3a3919476a33693af5e383
Kevin Tulus <kevintulussilitonga@gmail.com>
2021-06-18 19:13:45 +0700

    Database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('nis/nip');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->unsignedInteger('status')->nullable();
+            $table->unsignedInteger('role')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2021_06_04_152334_create_teachers_table.php
+++ database/migrations/2021_06_04_152334_create_teachers_table.php
@@ -17,7 +17,6 @@ class CreateTeachersTable extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict');
-            $table->unsignedBigInteger('nip');
             $table->timestamps();
         });
     }
